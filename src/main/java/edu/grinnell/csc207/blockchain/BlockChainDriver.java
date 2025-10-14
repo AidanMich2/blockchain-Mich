d63e042856ac4a3a4e0dbc3701686e6f73a3c5d9
Sam Beck <sambeck@Mac.grinnell.edu>
2025-10-13 22:18:04 -0500

    put in the rest of the functions to the driver

--- src/main/java/edu/grinnell/csc207/blockchain/BlockChainDriver.java
+++ src/main/java/edu/grinnell/csc207/blockchain/BlockChainDriver.java
@@ -40,6 +40,34 @@ public class BlockChainDriver {
                 bc.append (bc.mine (num));
                  System.out.println("Nonce? ");
             }
+            if (scan.next ().equals ("remove")){
+                if(bc.removeLast()){
+                    return;
+                }
+            }
+            if (scan.next ().equals ("check")){
+                if(bc.isValidBlockChain()){
+                    System.out.println ("Chain is valid!");
+                } else{
+                    System.out.println ("Chain is invalid!");
+                }
+            }
+            if (scan.next ().equals ("report")){
+                bc.printBalances();
+            }
+            if (scan.next ().equals ("help")){
+                System.out.println ("Valid commands:");
+                System.out.println ("   mine: discovers the nonce for a given transaction");
+                System.out.println ("   append: appends a new block onto the end of the chain");
+                System.out.println ("   remove: removes the last block from the end of the chain");
+                System.out.println ("   check: checks that the block chain is valid");
+                System.out.println ("   report: reports the balances of Alice and Bob");
+                System.out.println ("   help: prints this list of commands");
+                System.out.println ("   quit: quits the program");
+            }
+            if (scan.next ().equals ("quit")){
+                running = false;
+            }
         }
     }  
 }
